8b5e29293f346bbb898a628daf75868f20ae7382
aryasinha0 <159887158+aryasinha0@users.noreply.github.com>
2025-08-13 16:23:04 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="../images/brand_logo.png" alt="logo" />
+            <ul>
+                <li>Home</li>
+                <li>Location</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <button>Login</button>
+        </header>
+    )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                <div className="Btns">
+                    <button className="shop-btn">
+                        Shop Now
+                    </button>
+                    <button className="category">Category</button>
+                </div>
+                
+            </div>
+            <img src="../images/shoe_image.png" alt="shoe-image" />
+        </div>
+    )
+}
